ce8ccadb22662248045c79c073ca3b1e4ca24d3d
Slideman63 <73056372+Slideman63@users.noreply.github.com>
2020-10-18 10:00:38 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// ********** set date ************
+// select span
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+const header = document.querySelector('.navbar');
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#a599d1";
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+});
